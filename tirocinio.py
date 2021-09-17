26ecc08ecc9ad626add90e7831ac3134edb9f084
Luca Zanoni <luca.zanoni3@studio.unibo.it>
2021-09-17 08:49:49 +0200

    Update tirocinio.py
    add function to divide the df in cycles

--- tirocinio.py
+++ tirocinio.py
@@ -11,22 +11,24 @@ import pandas as pd
 import pylab as plt
 import datetime
 import scipy as sp
-from scipy.signal import argrelextrema
+
 
 path="D:/lucaz/OneDrive/Desktop/tirocinio/lavoro/MindsphereFleetManager"
 #%%
 #read Bson file
-with open(path+"/AQS_cycle10.bson", "rb") as rf:
+with open(path+"/AQS_cycle10002_phase1.bson", "rb") as rf:
     data = bson.decode(rf.read())
     
 #se il file è phase ha variabili normali e phase, cioè due etichette nel dict, altrimenti solo 1    
 datafr1=pd.DataFrame(data[list(data)[0]])
 if len(list(data))==2:
     datafr_phase=pd.DataFrame(data[list(data)[1]])
-#%%
-#read excel file
-datafr=pd.read_excel(path+"/excel_csv/c6378abbfb4b4c079a63dd5489bed1e6_AnalogFast.xlsx")
-datafrsl=pd.read_excel(path+"/excel_csv/c6378abbfb4b4c079a63dd5489bed1e6_AnalogSlow.xlsx")
+    
+    #%%
+with open('mapping_phasevariables.json') as json_file:
+    data_phase=json.load(json_file)
+phasevars=pd.DataFrame(data_phase['phaseIdVars'])
+
 #%%
 # read all the bson filename in the path
 #funziona
@@ -36,6 +38,11 @@ os.chdir(path)
 for file in glob.glob("*.bson"):
     files.append(file)
     
+#%%
+#read excel file
+df_fast=pd.read_excel(path+"/excel_csv/c6378abbfb4b4c079a63dd5489bed1e6_AnalogFast.xlsx")
+df_slow=pd.read_excel(path+"/excel_csv/c6378abbfb4b4c079a63dd5489bed1e6_AnalogSlow.xlsx")
+
 #%%
 #take time from iso 8601 data
 
@@ -56,11 +63,6 @@ def numbers_from_time(df,timename='_time'):
         t=timefromiso(time)
         timenum.append(time_to_num(t))
     return (np.array(timenum))#-timenum[0])
-
-    #%%
-with open('mapping_phasevariables.json') as json_file:
-    data_phase=json.load(json_file)
-phasevars=pd.DataFrame(data_phase['phaseIdVars'])
 
 
 
@@ -97,15 +99,143 @@ def mapping_var_big_than(df,namevar,namevar2,limit=None,timename='_time',title='
 #       plt.xlabel('time')
 #       plt.ylabel(namevar2)
 ##       
-#%%
 
-minima=[]   
-minima=argrelextrema(np.array(datafr3['analogFast3']), np.less)
 #%%
 #add to df a column of time in second
 def add_time_as_number(df,timename='_time'):
     timenumberdf=pd.DataFrame({"Time number":numbers_from_time(df,timename)})
     return(pd.concat([df,timenumberdf],axis=1))
+#%%
+
+#calc volume as itegral of flow
+
+def volume_from_flow(flows,times):
+    volume=[]
+    volume.append(0)
+    for i in range(0,len(flows)-1):
+        volume.append(volume[i]+(flows[i+1]+flows[i])*(times[i+1]-times[i])/(2*3600))
+    return volume
+
+#%%
+    """voglio selezionare una zona di punti, idealmente quelli di un ciclo, che sanno sopra un certo valore eludendo le fluttuazioni"""
+def selecting_cycle(arr, limit, n_point_under,n_point_over):
+    #arr=array of values
+    #limit=lower limit in which points are not good
+    #n_point_under=number of point under which no start cycle index is taken
+    #n_point_over=number of point under which no end cycle index is taken
+    #return=starting and ending cycle indices 
+        indices=[]
+        index_start=0
+        index_end=0
+        
+#        h=False
+        k=False
+        count1=0 #count1 count the numbers over limit
+        count2=0
+        for i in range(0,len(arr)):
+            if arr[i]>limit:
+#                h=True
+                count1=count1+1
+                count2=0
+            else:
+#                h=False
+                count1=0
+                count2=count2+1
+            if (count1==n_point_under  and not(k)):
+                index_start=i-n_point_under
+                k=True
+            if (count2==n_point_over and k):
+                index_end=i-n_point_over
+                indices.append([index_start,index_end])
+                index_start=0
+                index_end=0
+                k=False
+        return indices
+    #da correggere
+
+#%%
+"""linear interpolation"""
+"""np.interp(time1,time2, density)"""
+#%%
+#pairing slow and fast variable adding missing points in slow variables
+#slow variables have a minor sampling rate, so we need to interpolate the missing points
+#how? or linear interpolation or finding some low of the points and generating missing points from those curves
+"""def linear_interpolation(x1,y1,x2,y2,xp):
+    return ((xp-x2)/(x1-x2)*y1-(xp-x1)/(x1-x2)*y2)
+
+
+def linear_variables_interpolation(var_array1,time1,var_array2,time2):#var_array1=more dense, var_array2= less dense
+    var2_interpolated=np.zeros(len(time1))
+    arr_same_values=np.in1d(time1,time2)#boolean array with True if the element of time1 is in time2
+    for i in range(0,len(time1)):
+        if arr_same_values[i]:
+            var2_interpolated[i]=var_array2[i] #filled var2_interpolated with the values of time1 that are present in time2
+#            
+#    index_prev_true=0
+#    index_next_true=0
+#    #find the previous and next values in common between time 1 and 2 end interpolate points between the two
+#    for i in range(1,len(time1)):
+#        if arr_same_values[i]: 
+#            index_prev_true=i
+#        j=0
+#        if (arr_same_values[i]):
+#            j=i
+#            while (not(arr_same_values[j])and (j<len(time1))):
+#                j=j+1
+#            if(j<len(time1)):
+#                index_next_true=j
+#                #here i will use also all the other interpolation curve
+#                x1=time1[index_prev_true]
+#                y1=var_array1[index_prev_true]
+#                x2=time1[index_next_true]
+#                y2=var_array1[index_next_true]
+#                var2_interpolated[i]=linear_interpolation(x1,y1,x2,y2,time2[i])
+#            else:
+#                var2_interpolated[i]=0
+    return var2_interpolated
+        """
+        
+"""BASTAVA UNA FUNZIONE DI NUMPY"""
+"""np.interp(time1,time2, density)"""
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
+"""
+PROVE FUNZIONI E PER CAPIRE I DATI
 
 #%%
 #division of df analogFast about 1-9-2021 in cycles
@@ -144,8 +274,13 @@ plt.scatter(df_1_9['Time number'],df_1_9['analogFast1'],marker='.')
 plt.scatter(df_1_9['Time number'],df_1_9['analogFast3'],marker='.')
 
 
+
 #%%
 
-CALCOLARE IL VOLUME DE
+fig, axs = plt.subplots(3, 1)
+axs[0].scatter(datafr1['Time number'],datafr1['Analogs.analog1'],marker='.')
+axs[1].scatter(datafr1['Analogs.analog1'],datafr1['Analogs.analog3'],marker='.')
+axs[2].scatter(datafr1['Time number'],datafr1['Analogs.analog3'],marker='.')
 
+"""
 
